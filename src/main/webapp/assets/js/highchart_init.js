b8776d1cb29c23eb4615685517f96a71e7b4de6e
vissul <gvissul@gmail.com>
2013-12-02 13:32:50 +0800

    added data to highchart

--- src/main/webapp/assets/js/highchart_init.js
+++ src/main/webapp/assets/js/highchart_init.js
@@ -1,63 +1,44 @@
-$(function () {
-    $('#highcharts').highcharts({
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: '温度图表'
-        },
-        xAxis: {
-            type: '长度',
-            dateTimeLabelFormats: { // don't display the dummy year
-                month: '%e. %b',
-                year: '%b'
-            }
-        },
-        yAxis: {
-            title: {
-                text: '温度'
-            },
-            min: 0
-        },
-        tooltip: {
-            formatter: function() {
-                    return '<b>'+ this.series.name +'</b><br/>'+
-                    Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' m';
-            }
-        },
-        
-        series: [{
-//            name: 'Winter 2007-2008',
-            // Define the data points. All series have a dummy year
-            // of 1970/71 in order to be compared on the same x axis. Note
-            // that in JavaScript, months start at 0 for January, 1 for February etc.
-            data: [
-                [Date.UTC(1970,  9, 27), 0   ],
-                [Date.UTC(1970, 10, 10), 0.6 ],
-                [Date.UTC(1970, 10, 18), 0.7 ],
-                [Date.UTC(1970, 11,  2), 0.8 ],
-                [Date.UTC(1970, 11,  9), 0.6 ],
-                [Date.UTC(1970, 11, 16), 0.6 ],
-                [Date.UTC(1970, 11, 28), 0.67],
-                [Date.UTC(1971,  0,  1), 0.81],
-                [Date.UTC(1971,  0,  8), 0.78],
-                [Date.UTC(1971,  0, 12), 0.98],
-                [Date.UTC(1971,  0, 27), 1.84],
-                [Date.UTC(1971,  1, 10), 1.80],
-                [Date.UTC(1971,  1, 18), 1.80],
-                [Date.UTC(1971,  1, 24), 1.92],
-                [Date.UTC(1971,  2,  4), 2.49],
-                [Date.UTC(1971,  2, 11), 2.79],
-                [Date.UTC(1971,  2, 15), 2.73],
-                [Date.UTC(1971,  2, 25), 2.61],
-                [Date.UTC(1971,  3,  2), 2.76],
-                [Date.UTC(1971,  3,  6), 2.82],
-                [Date.UTC(1971,  3, 13), 2.8 ],
-                [Date.UTC(1971,  4,  3), 2.1 ],
-                [Date.UTC(1971,  4, 26), 1.1 ],
-                [Date.UTC(1971,  5,  9), 0.25],
-                [Date.UTC(1971,  5, 12), 0   ]
-            ]
-        }]
-    });
+$(function() {
+	var cables = new Array();
+	$.ajax({
+		url : "http://localhost:8080/dts/monitor/19.json",
+		type : "get",
+		dataType : "json",
+		success : init_data
+	});
+	function init_data(data) {
+		for (var i = 0; i < data.length; i++) {
+			cables[i] = [ data[i].length, data[i].temperature ];
+		}
+		$('#highcharts').highcharts({
+			chart : {
+				type : 'spline'
+			},
+			title : {
+				text : '温度图表'
+			},
+			xAxis : {
+				type : '长度',
+				min : 0,
+				max : 1000
+			},
+			yAxis : {
+				title : {
+					text : '温度'
+				},
+				min : 0,
+				max : 100
+			},
+			// tooltip: {
+			// formatter: function() {
+			// return '<b>'+ this.series.name +'</b><br/>'+
+			// Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' m';
+			// }
+			// },
+			series : [ {
+				data : cables
+			} ]
+		});
+	}
+
 });
